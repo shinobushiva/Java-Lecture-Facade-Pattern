c8ad90f36191cbbddf9e969f1d424850c5f8c00c
Shinobu Izumi <izumi@cis.sojo-u.ac.jp>
2016-11-25 13:49:23 +0900

    コメント修正

--- src/lesson/EnumLibrarianTest.java
+++ src/lesson/EnumLibrarianTest.java
@@ -9,7 +9,6 @@ public class EnumLibrarianTest {
 
 		Librarian<String> librarian = new CommentLibrarian();
 
-		// 中村くんに昆虫図鑑の場所を聞く
 		String bookName = "どんと来い、超常現象";
 		System.out.println(bookName + " は" + librarian.searchBook(bookName));
 		bookName = "どんと来い、超常現象２";
--- src/patterned/CustomerTest.java
+++ src/patterned/CustomerTest.java
@@ -5,7 +5,6 @@ public class CustomerTest {
 
 		Librarian<String> librarian = new CommentLibrarian();
 
-		// 中村くんに昆虫図鑑の場所を聞く
 		String bookName = "どんと来い、超常現象";
 		System.out.println(bookName + " は" + librarian.searchBook(bookName));
 		bookName = "どんと来い、超常現象２";
